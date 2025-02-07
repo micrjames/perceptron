3065bb3801b54ef05234af7add8375cdf6caaa38
Michael James <micrjames921@gmail.com>
2025-02-07 16:04:30 -0700

    defined 'Animation'

---
+++ src/Animation.ts
@@ -0,0 +1,12 @@
+import { Circle } from "./Circle";
+
+export class Animation {
+   constructor(circle: Circle, colors: string[], intervalMS: number = 1000) {
+   }
+
+   start() {
+   }
+
+   stop() {
+   }
+}
